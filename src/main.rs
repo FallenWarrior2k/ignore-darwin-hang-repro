0361b962cdf5006441cf82a95c24dd9cf3342916
Simon Engmann <simon.engmann@tu-dortmund.de>
2019-11-15 00:39:32 +0100

    Second attempt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use ignore::WalkBuilder;
 
 fn main() {
-    for x in WalkBuilder::new("/")
-        .max_depth(Some(2)).same_file_system(true).build() {
+    for x in WalkBuilder::new("/").same_file_system(true).build() {
         match x {
             Ok(entry) => println!("{:?}", entry),
             Err(err) => eprintln!("Error: {:?}", err)

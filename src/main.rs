69d15b89f0c5cb3af330c88518de7ea48ebcff0a
Simon Engmann <simon.engmann@tu-dortmund.de>
2019-11-15 00:44:19 +0100

    Third attempt

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use ignore::WalkBuilder;
 
 fn main() {
     for x in WalkBuilder::new("/").same_file_system(true).build() {
-        match x {
-            Ok(entry) => println!("{:?}", entry),
-            Err(err) => eprintln!("Error: {:?}", err)
+        if let Err(err) = x {
+            eprintln!("Error: {:?}", err);
         }
     }
+
+    println!("Outside walk.")
 }
